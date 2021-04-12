984f06c9b69b51c1b34b9f356f6ba2ae9a3c7c00
Vitaliy Povstenko <vitaly.povstenko@gmail.com>
2021-04-12 19:55:53 +0300

    verify

--- solution.py
+++ solution.py
@@ -3,15 +3,17 @@ def solution(x, y):
     F = int(y)
     generations = 0
 
-    while M != 0 and F != 0:
-        if M < F:
-            generations += F/M
-            F = F % M
+    while M != F:
+        if M > F:
+            subs = (M-F)//F + ((M-F) % F > 0)
+            generations += subs
+            M = M - subs * F
         else:
-            generations += M/F
-            M = M % F
+            subs = (F-M)//M + ((F-M) % M > 0)
+            generations += subs
+            F = F - subs * M
 
-    return str(generations - 1) if(x, y) == (1, 1) else 'impossible'
+    return str(generations) if(M, F) == (1, 1) else 'impossible'
 
 
 
