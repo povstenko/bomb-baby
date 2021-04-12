9cb22953601de3bad79dac0a28630f0180b2db37
Vitaliy Povstenko <vitaly.povstenko@gmail.com>
2021-04-12 19:47:37 +0300

    First test

--- solution.py
+++ solution.py
@@ -1,2 +1,19 @@
 def solution(x, y):
-    #​ ​Your​ ​code​ ​here
+    M = int(x)
+    F = int(y)
+    generations = 0
+
+    while M != 0 and F != 0:
+        if M < F:
+            generations += F/M
+            F = F % M
+        else:
+            generations += M/F
+            M = M % F
+
+    return str(generations - 1) if(x, y) == (1, 1) else 'impossible'
+
+
+
+print(solution('4', '7'))
+print(solution('2', '1'))
